acee9ddd7de50000dfc0f226002c8ac30ef61846
George Chanturidze <georgechanturidze@georges-mbp.attlocal.net>
2018-07-07 16:02:57 -0700

    scraped articles displaying

--- controllers/headline.js
+++ controllers/headline.js
@@ -15,11 +15,7 @@ exports.displayAllHeadlines = function (req, res) {
 
 exports.clearHeadlines = function (req, res) {
   // Grab every document in the Articles collection
-  db.Headline.find({})
-    .then(function(dbHeadline) {
-      // If we were able to successfully find Articles, send them back to the client
-      res.json(dbHeadline);
-    })
+  db.Headline.deleteMany({})
     .catch(function(err) {
       // If an error occurred, send it to the client
       res.json(err);
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -44,8 +44,8 @@ $(document).ready(function() {
       var cardHeader = $("<div class='card-header'>").append(
         $("<h3>").append(
           $("<a class='article-link' target='_blank' rel='noopener noreferrer'>")
-            .attr("href", article.url)
-            .text(article.headline),
+            .attr("href", article.link)
+            .text(article.title),
           $("<a class='btn btn-success save'>Save Article</a>")
         )
       );
