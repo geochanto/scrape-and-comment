cedf554cd8cfd3f776d35efb7c9ba1ecbbc24f1d
George Chanturidze <georgechanturidze@georges-mbp.attlocal.net>
2018-07-08 19:05:12 -0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# scrape-and-comment
+# Scrape and Commet
+This webapp lets users scrape articles from [Reuters technology news page](https://www.reuters.com/news/archive/technologyNews).
+
+Articles can be saved (and unsaved), and notes can be added for saved articles.
+
+Articles can also be "cleared". This deletes all saved and unsaved articles, as well as associated notes.
+
+You can access a hosted demo here: <a target="_blank" href="https://scrape-and-comment.herokuapp.com">https://scrape-and-comment.herokuapp.com</a>
+
+## Technologies Used
+1. MVC Framework
+2. Node.js
+3. MongoDB & Mongoose ODM
+4. Heroku & mLab (for deployment)
+
+## Get started
+To get started with this project, do one of the following:
+
+1. Using git from command line, `git clone git@github.com:geochanto/scrape-and-comment.git` 
+
+2. Download the zip archive: https://github.com/geochanto/scrape-and-comment/archive/master.zip
+
+3. Create a fork at https://github.com/geochanto/scrape-and-comment
--- routes/api.js
+++ routes/api.js
@@ -5,26 +5,21 @@ const fetch_controller = require('../controllers/fetch');
 const headline_controllers = require('../controllers/headline');
 const note_controller = require('../controllers/note');
 
+//scraping route
 router.get("/api/fetch", fetch_controller.fetchHeadlines);
 
+//headline routes
 router.delete("/api/clear", headline_controllers.clearHeadlines);
-
 router.get("/api/headlines", headline_controllers.displayAllHeadlines);
-
 router.get("/api/headlines/saved", headline_controllers.displaySavedHeadlines);
-
 router.get("/api/headlines/unsaved", headline_controllers.displayUnsavedHeadlines);
-
 router.get("/api/headlines/:id", headline_controllers.displayOneHeadline);
-
 router.put("/api/headlines/save/:id", headline_controllers.saveHeadline);
-
 router.put("/api/headlines/unsave/:id", headline_controllers.unsaveHeadline);
 
+//note routes
 router.get("/api/notes/:id", note_controller.displayNotes);
-
 router.post("/api/notes", note_controller.addNote);
-
 router.delete("/api/notes/:id", note_controller.deleteNote);
 
 
